598c0e8ae0436fc9e1fb011d0a21643582138dee
Raphael Scheible <raphael.scheible@uniklinik-freiburg.de>
2021-05-05 15:56:05 +0200

    refined preclean rules

--- workflow_filterdata/scripts/clean_it.sh
+++ workflow_filterdata/scripts/clean_it.sh
@@ -5,6 +5,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 # fix encodings, remove bad encodings and remove special symbols
 ${DIR}/clean_text.py -p ${1} \
-    | grep -P -v '^\s*$' \
     | perl -CSD -Mutf8 -pe \
-        's/\p{Modifier_Symbol}+|\p{Other_Symbol}+|\p{Unassigned}+|\p{Private_Use}+|\p{Surrogate}+|\p{Mark}+|\p{Other_Letter}+//g'
+        's/\p{Modifier_Symbol}+|\p{Other_Symbol}+|\p{Unassigned}+|\p{Private_Use}+|\p{Surrogate}+|\p{Mark}+|\p{Other_Letter}+//g' \
+    | grep -axv '.*' \
+    | grep -va '�'
