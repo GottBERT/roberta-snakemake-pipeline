9ce486de4aee5f6e469854d8ada9b52bb19adec8
Raphael Scheible <raphael.scheible@tum.de>
2022-07-07 11:22:08 +0200

    added comments

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ import pickle
 
 nltk.download("stopwords")
 
-in_file = "output/filtered.txt"
+in_file = "files/example/de_dedup.txt"
 out_file = "output/filtered_final.txt"
 pickle_out = "output/result.pickle"
 language = "German"
@@ -40,32 +40,48 @@ with open(in_file, mode="r", encoding="utf-8") as f_in:
     word_tokens = nltk.tokenize.word_tokenize(document)
     
     if len(word_tokens) > min_tokens:
+        # tokens without stopwords
         filtered_sentence = [w for w in word_tokens if not w in stop_words]
         
-        stopword_ratio = len(filtered_sentence)/len(word_tokens)
+        # compute stopword ratio, i.e. 1-|tokens without stop words|/|tokens of document|
+        stopword_ratio = 1-len(filtered_sentence)/len(word_tokens)
         
+        # number of unique tokens
         num_unique_tokens_filtered = len(set(filtered_sentence))
+        
+        # number of tokens of the original document
         num_tokens_original = len(word_tokens)
         
+        # apply word tokenizer to get words
         words = word_tokenizer.tokenize(document)
+        
+        # compute puntuation, i.e. difference of word tokens and original tokens
         punctuation = [w for w in word_tokens if not w in words]
         
+        # get tokens starting with upper case character
         upper = [w for w in word_tokens if w[0].isupper()]
     
-      
+        
+        # compute how many puncuations occur in document
         num_punctuation = len(punctuation)
         
+        # compute puntuation ratio, i.e. |puncuation tokens|/|tokens of document|
         punctuation_ratio = num_punctuation/num_tokens_original
       
+        # compute token ratio, which describes the occurence of repeating words
         token_ratio = num_unique_tokens_filtered/num_tokens_original
 
+        # number of tokens which start with a upper case character
         num_upper = len(upper)
         
-        if num_punctuation > 0:  
+        # after puctuation, in German, one continues to write in capital
+        # TODO: is it possible to scale it between 1 and 0
+        if num_punctuation > 0:
           upper_to_punct_ratio = num_upper/num_punctuation
         else:
           upper_ratio = 0
       
+        # ratio between upper word tokens and tokens in general, i.e. |upper case tokens|/|tokens of document|
         upper_ratio = num_upper/num_tokens_original
       
         if num_tokens_original > 0:
