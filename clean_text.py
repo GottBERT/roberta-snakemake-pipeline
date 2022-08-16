566ef89c1208776ede3109c62a9087ed75df8916
Henry He <henry.he@tum.de>
2022-08-16 14:41:52 +0200

    Add clean_text.py

---
+++ clean_text.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+from cleantext import clean
+
+INFILE = "files/example/de_dedup.txt"
+OUTFILE = "de_dedup_cleaned.txt"
+
+with open(INFILE, "r") as r, open(OUTFILE, "w") as w:
+    for line in r:
+        text = clean(line,
+                     lower=False,
+                     no_urls=True,
+                     no_emails=True,
+                     no_phone_numbers=True,
+                     no_emoji=True,
+                     replace_with_url="",
+                     replace_with_email="",
+                     replace_with_phone_number="",
+                     lang="de")
+        w.write(text + "\n")
