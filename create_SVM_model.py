0c2e45079570fcf209452034456c481fb3ae49ed
Raphael Scheible <raphael.scheible@tum.de>
2023-03-23 16:34:36 +0100

    script is working

--- create_SVM_model.py
+++ create_SVM_model.py
@@ -8,76 +8,106 @@ import numpy as np
 # which is more efficient for sklearn models
 from joblib import load, dump
 import argparse
-
-parser = argparse.ArgumentParser(prog='compute_ratios')
-parser.add_argument('--train', dest="in_train", help='input train file', required=True)
-parser.add_argument('--test', dest="in_test", help='input test file', required=True)
-parser.add_argument('--in', dest="in_pickle", help='input pickle file', required=True)
-parser.add_argument('--out', dest="out_model", help='output joblib file',required=False, default='oneclasssvm.joblib')
-parser.add_argument('--nu',  help='nu', required=True)
-parser.add_argument('--language', required=False, default='German')
-args = parser.parse_args()
-
-# Select a subset of ratios as model features
-df = pd.read_pickle(args.out_pickle)
-df = df[["stopword_ratio", "punctuation_ratio", "token_ratio", "upper_ratio", "upper_to_punct_ratio"]]
-df = df.astype(float)
-
-# 
-X = df.to_numpy()
-idx = range(len(X))
-X_train, X_test, idx_train, idx_test = model_selection.train_test_split(X, idx, test_size=0.2)
-clf = svm.OneClassSVM(nu=nu)
-clf.fit(X_train)
-
-y_pred_train = clf.predict(X_train)
-y_pred_test = clf.predict(X_test)
-
-labels = pd.DataFrame(list(zip(idx_test, y_pred_test)), columns=["index", "class"])
-labels.set_index('index', inplace=True)
-
-d = pd.read_pickle("ratios.pkl")["original_text"].iloc[idx_test]
-pred = pd.concat([d, labels], axis=1)
-
-# pred[pred['class'] == -1].to_csv('pred.csv')
+import json
 
 from sklearn.metrics import (
     confusion_matrix,
     accuracy_score,
     precision_score,
     recall_score,
-    f1_score
+    f1_score,
+    classification_report, 
+    matthews_corrcoef
 )
+
+parser = argparse.ArgumentParser(prog='compute_ratios')
+parser.add_argument('--train', dest="in_train", help='input train pickle file', required=True)
+parser.add_argument('--test', dest="in_test", help='input test pickle file', required=True)
+parser.add_argument('--gt', dest="in_groundtruth", help='path to ground truth json (label studio minimal format)', required=True)
+parser.add_argument('--out', dest="out_model", help='output joblib file',required=False, default='oneclasssvm.joblib')
+
+# list for nu can be created by e.g. print(','.join(str(x.round(3)) for x in np.arange(0.01,0.1, 0.005)))
+parser.add_argument('--nu',  dest='nu', required=True, help='multiple values possible als comma separated list')
+parser.add_argument('--language', required=False, default='German')
+args = parser.parse_args()
 
 # load labelled data
 pd_label_studio = pd.read_json('label_studio-min.json')
 
+# Select a subset of ratios as model features
+cols = ["stopword_ratio", "punctuation_ratio", "token_ratio", "upper_ratio", "upper_to_punct_ratio"]
+df_train = pd.read_pickle(args.in_train)[cols].astype(float)
+df_test = pd.read_pickle(args.in_test)[cols].astype(float)
 
-# model evaluation
-# merge with groundtruth data
-df_merge = pd.merge(pd_label_studio.reset_index()[['index','sentiment']], pred.reset_index(), on='index')
-df_merge['sentiment'] = df_merge['sentiment'].replace('mixed', -1).replace('spam', -1).replace('clean', 1)
+# 
+X_test = df_test.to_numpy()
+idx_test = df_test.reset_index()['index'].to_numpy()
 
-y_test = df_merge['sentiment']
-y_test_predictions = df_merge['class']
+X_train = df_train.to_numpy()
+idx_train = df_train.reset_index()['index'].to_numpy()
 
-conf_matrix = confusion_matrix(y_test, y_test_predictions)
+df_eval = pd.DataFrame(columns=['nu', 'accuracy', 'precision', 'recall', 'weighted_f1_score', 'mcc', 'report', 'model'])
+
+# estimate best SVM given nu
+for nu in list(map(lambda e: float(e), args.nu.split(","))):
+    # create and train model
+    model = svm.OneClassSVM(nu=nu)
+    model.fit(X_train)
+
+    y_pred_test = model.predict(X_test)
+
+    labels = pd.DataFrame(list(zip(idx_test, y_pred_test)), columns=["index", "class"])
+    labels.set_index('index', inplace=True)
+
+    d = pd.read_pickle("ratios.pkl")["original_text"].iloc[idx_test]
+    pred = pd.concat([d, labels], axis=1)
+
+    # model evaluation
+    # merge with groundtruth data
+    df_merge = pd.merge(pd_label_studio.reset_index()[['index','sentiment']], pred.reset_index(), on='index')
+    df_merge['sentiment'] = df_merge['sentiment'].replace('mixed', -1).replace('spam', -1).replace('clean', 1)
+
+    y_test = df_merge['sentiment']
+    y_test_predictions = df_merge['class']
+
+    # confusion matrix
+    conf_matrix = confusion_matrix(y_test, y_test_predictions)
+
+    # compute some metrics
+    accuracy = accuracy_score(y_test, y_test_predictions)
+    precision = precision_score(y_test, y_test_predictions)
+    recall = recall_score(y_test, y_test_predictions)
+    f1score = f1_score(y_test, y_test_predictions, average='weighted')
+
+    # https://biodatamining.biomedcentral.com/articles/10.1186/s13040-023-00322-4
+    # https://bmcgenomics.biomedcentral.com/articles/10.1186/s12864-019-6413-7
+    mcc = matthews_corrcoef(y_test, y_test_predictions)
+
+    # append data to pandas dataframe
+    report = classification_report(y_test, y_test_predictions, target_names=['dirty', 'clean'], output_dict=True)
+    list_row = [nu, accuracy, precision, recall, f1score, abs(mcc), report, model]
+    df_eval.loc[len(df_eval)] = list_row
 
 
-# Metrics
-from sklearn.metrics import classification_report
+    # print(f"Accuracy = {accuracy.round(4)}")
+    # print(f"Precision = {precision.round(4)}")
+    # print(f"Recall = {recall.round(4)}")
+    # print(f"F1 Score = {f1score.round(4)}")
+    # print(f"MCC = {mcc.round(4)}")
 
-accuracy = accuracy_score(y_test, y_test_predictions)
-precision = precision_score(y_test, y_test_predictions)
-recall = recall_score(y_test, y_test_predictions)
-f1score = f1_score(y_test, y_test_predictions)
+    # print(classification_report(y_test, y_test_predictions, target_names=['dirty', 'clean']))
 
-print(f"Accuracy = {accuracy.round(4)}")
-print(f"Precision = {precision.round(4)}")
-print(f"Recall = {recall.round(4)}")
-print(f"F1 Score = {f1score.round(4)}")
+# estimate best model
+o_best_model = df_eval.iloc[df_eval['mcc'].idxmax()]
 
-print(classification_report(y_test, y_test_predictions, target_names=['dirty', 'clean']))
+# save best model
+best_model = o_best_model.pop('model')
+metrics = o_best_model
 
-# save model
-dump(clf, out_model)
+# print(metrics)
+
+# save data
+dump(best_model, args.out_model)
+metrics.to_json('metrics.json', orient='index', indent=2)
+
+df_eval[metrics.keys()].to_csv('training_log.csv', index=False)
