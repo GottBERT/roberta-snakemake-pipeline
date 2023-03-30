35b90237b748c85f74fca47e76b36e2f60d306ac
Raphael Scheible <raphael.scheible@tum.de>
2023-03-30 14:36:21 +0200

    fixed ratios

--- compute_ratios.py
+++ compute_ratios.py
@@ -39,16 +39,16 @@ with open(args.in_file, mode="r", encoding="utf-8") as f_in:
     word_tokens = nltk.tokenize.word_tokenize(document, language=language)
     
     # tokens without stopwords
-    filtered_sentence = [w for w in word_tokens if not w in stop_words]
+    filtered_tokens = [w for w in word_tokens if not w in stop_words]
     
     # compute stopword ratio, i.e. 1-|tokens without stop words|/|tokens of document|
     if len(word_tokens) > 0:
-        stopword_ratio = 1-len(filtered_sentence)/len(word_tokens)
+        stopword_ratio = 1-len(filtered_tokens)/len(word_tokens)
     else:
         stopword_ratio = 0
     
     # number of unique tokens
-    num_unique_tokens_filtered = len(set(filtered_sentence))
+    num_unique_tokens_filtered = len(set(filtered_tokens))
     
     # number of tokens of the original document
     num_tokens_original = len(word_tokens)
@@ -86,17 +86,17 @@ with open(args.in_file, mode="r", encoding="utf-8") as f_in:
     if num_punctuation > 0 and num_upper > 0:
       upper_to_punct_ratio = (num_upper/num_punctuation) / num_upper
     else:
-      upper_ratio = 0
+      upper_to_punct_ratio = 0
   
     # ratio between upper word tokens and tokens in general, i.e. |upper case tokens|/|tokens of document|
     if num_tokens_original > 0:
         upper_ratio = num_upper/num_tokens_original
     else:
-        num_tokens_original = 0
+        upper_ratio = 0
   
     if num_tokens_original > 0:
       df_result = df_result.append({
-          "filtered_tokens": " ".join(filtered_sentence),
+          "filtered_tokens": " ".join(filtered_tokens),
           "original_tokens": " ".join(word_tokens),
           "original_text": document.rstrip('\n'),
           "stopword_ratio": stopword_ratio,
