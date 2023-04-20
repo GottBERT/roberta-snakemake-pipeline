57fb495a6b0c0751e2493254128733521e9b84df
Raphael Scheible <raphael.scheible@tum.de>
2023-04-20 15:49:51 +0200

    fixed binarization

--- workflow_preprocess/scripts/preprocess.sh
+++ workflow_preprocess/scripts/preprocess.sh
@@ -6,22 +6,18 @@ dir_out=${2}
 if [[ -f "${dir_data}/dict.txt" ]]; then
   fairseq-preprocess \
     --only-source \
-    --cpu \
     --srcdict ${dir_data}/dict.txt \
     --trainpref ${dir_data}/train.bpe \
     --validpref ${dir_data}/valid.bpe \
     --testpref ${dir_data}/test.bpe \
-    --bpe hf_byte_bpe \
     --destdir ${dir_out} \
     --workers 60
 else 
   fairseq-preprocess \
     --only-source \
-    --cpu \
     --trainpref ${dir_data}/train.bpe \
     --validpref ${dir_data}/valid.bpe \
     --testpref ${dir_data}/test.bpe \
-    --bpe hf_byte_bpe \
     --destdir ${dir_out} \
     --workers 60
 fi
