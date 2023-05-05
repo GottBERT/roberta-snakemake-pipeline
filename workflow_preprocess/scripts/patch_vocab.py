7acf06730ba052092718a42feb6a96a7e5752ed2
Raphael Scheible <raphael.scheible@tum.de>
2023-05-05 20:12:56 +0200

    add patch vocab

---
+++ workflow_preprocess/scripts/patch_vocab.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+
+import argparse
+import json
+import pandas as pd
+
+from shutil import copyfile
+
+from os import path
+
+
+def main():
+  parser = argparse.ArgumentParser(description='')
+  parser.add_argument('--dir', required=True)
+  parser.add_argument('--dict_file', required=True)
+  args = parser.parse_args()
+
+  # create backup if not exists
+  if not path.isfile(path.join(args.dir, 'vocab.json.bu')):
+    print('create backup of vocab.json')
+    copyfile(path.join(args.dir, 'vocab.json'), path.join(args.dir, 'vocab.json.bu'))
+
+    json_file = 'vocab.json'
+  else:
+    json_file = 'vocab.json.bu'
+
+  # read dict.txt
+  dictionary = pd.read_csv(path.join(args.dict_file), delimiter=" ", header=None, names=['entry', 'count'])
+
+  with open(path.join(args.dir, json_file)) as f_json_file:
+    data = json.load(f_json_file)
+
+  last_index=data[list(data)[-1]]
+
+  # get madeup words
+  madeupwords = dictionary[
+    dictionary['entry'].astype("str").str.startswith("madeupword")
+  ]
+
+  for madeupword in list(madeupwords['entry']):
+    last_index = last_index+1
+
+    data[madeupword] = last_index
+    
+  data['<mask>'] = last_index+1
+
+  with open(path.join(args.dir, 'vocab.json'), 'w', encoding='utf-8') as f:
+    json.dump(data, f)
+
+if __name__ == '__main__':
+  main()
