e02e8fc46575249757c6b42ad523a17cef705850
Raphael Scheible <raphael.scheible@tum.de>
2024-01-10 19:45:13 +0100

    adjust parameters

--- pre-training/gpu/train.sh
+++ pre-training/gpu/train.sh
@@ -8,14 +8,17 @@ WARMUP_UPDATES=10000          # Warmup the learning rate over this many updates
 PEAK_LR=0.0004                # Peak learning rate, adjust as needed
 TOKENS_PER_SAMPLE=512         # Max sequence length
 LOG_INTERVAL=25               # Log interval. Don't do 1, it slows things down
-MAX_SENTENCES=8              # Number of sequences per batch (batch size)
-UPDATE_FREQ=8                 # Increase the batch size 2x
+MAX_SENTENCES=64              # Number of sequences per batch (batch size)
+UPDATE_FREQ=16                # Increase the batch size 2x
 
 
-DATASET="clean-small"
-DATA_DIR="./$DATASET"
-SAVE_DIR="./checkpoints"
-WORLD_SIZE=1
+DIR_OUTPUT="/data/norberto"
+
+DATA_DIR="/data/norberto/bin"
+SAVE_DIR="$DIR_OUTPUT/checkpoints"
+LOG_DIR="$DIR_OUTPUT/logs"
+
+WORLD_SIZE=8
 
 fairseq-train $DATA_DIR \
   --distributed-world-size $WORLD_SIZE \
@@ -24,7 +27,6 @@ fairseq-train $DATA_DIR \
   --task masked_lm \
   --criterion masked_lm \
   --optimizer adam \
-  --num-workers 4 \
   --adam-betas '(0.9,0.98)' \
   --adam-eps 1e-6 \
   --clip-norm 0.0 \
@@ -39,6 +41,7 @@ fairseq-train $DATA_DIR \
   --dropout 0.1 --attention-dropout 0.1 --weight-decay 0.01 \
   --batch-size $MAX_SENTENCES --update-freq $UPDATE_FREQ \
   --skip-invalid-size-inputs-valid-test \
-  --save-interval-updates 25 \
-  --bpe hf_byte_bpe \
+  --save-interval-updates $SAVE_INTERVAL_UPDATES \
+  --tensorboard-logdir $LOG_DIR \
   --max-update $TOTAL_UPDATES &> training-$(date +"%Y-%m-%d_%H-%M-%S").log
+#   --num-workers 4 \
