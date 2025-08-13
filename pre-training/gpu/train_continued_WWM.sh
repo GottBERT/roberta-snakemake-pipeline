5a382bf18fdf558f5a4bdbdd61da16c95b62f8ba
Raphael Scheible-Schmitt <mail@raphiniert.com>
2025-08-13 21:24:35 +0200

    add continued WWM

--- pre-training/gpu/train_continued_WWM.sh
+++ pre-training/gpu/train_continued_WWM.sh
@@ -6,22 +6,30 @@ export XLA_UNEVEN_HEARTBEAT_TIMEOUT=7200
 TOTAL_UPDATES=100000          # Total number of training steps
 SAVE_INTERVAL_UPDATES=100000  # Save intervals (should be multiple of total updates)
 WARMUP_UPDATES=10000          # Warmup the learning rate over this many updates
-PEAK_LR=0.0004                # Peak learning rate, adjust as needed
+PEAK_LR=0.0007                # Peak learning rate, adjust as needed
 TOKENS_PER_SAMPLE=512         # Max sequence length
 LOG_INTERVAL=25               # Log interval. Don't do 1, it slows things down
-MAX_SENTENCES=64              # Number of sequences per batch (batch size)
-UPDATE_FREQ=16                # Increase the batch size 2x
+MAX_SENTENCES=32              # Number of sequences per batch (batch size)
+UPDATE_FREQ=32                # Increase the batch size 2x
 
 
-DIR_OUTPUT="/data/norberto/base"
+DIR_OUTPUT="/data/geistbert/model/"
 
-DATA_DIR="/data/norberto/bin"
+DATA_DIR="/data/geistbert/output/bin"
+BPE=hf_byte_bpe
+
+RESTORE_CHECKPOINT=/data/geistbert/fairseq_checkpoints/large_clean/checkpoint_best.pt
+
 SAVE_DIR="$DIR_OUTPUT/checkpoints"
 LOG_DIR="$DIR_OUTPUT/logs"
+
+export BPE_VOCAB="$DATA_DIR/vocab.json"
+export BPE_MERGES="$DATA_DIR/merges.txt"
 
 WORLD_SIZE=8
 
 fairseq-train $DATA_DIR \
+  --restore-file $RESTORE_CHECKPOINT \
   --distributed-world-size $WORLD_SIZE \
   --log-format json \
   --log-interval $LOG_INTERVAL \
@@ -30,12 +38,15 @@ fairseq-train $DATA_DIR \
   --optimizer adam \
   --adam-betas '(0.9,0.98)' \
   --adam-eps 1e-6 \
+  --bpe $BPE \
   --clip-norm 0.0 \
-  --arch roberta_base \
-  --sample-break-mode none \
+  --arch roberta_large \
+  --sample-break-mode complete \
   --tokens-per-sample $TOKENS_PER_SAMPLE \
   --lr-scheduler polynomial_decay \
   --lr $PEAK_LR \
+  --fp16 \
+  --mask-whole-words \
   --save-dir $SAVE_DIR \
   --warmup-updates $WARMUP_UPDATES \
   --total-num-update $TOTAL_UPDATES \
@@ -44,5 +55,7 @@ fairseq-train $DATA_DIR \
   --skip-invalid-size-inputs-valid-test \
   --save-interval-updates $SAVE_INTERVAL_UPDATES \
   --tensorboard-logdir $LOG_DIR \
+  --restore-file $RESTORE_CHECKPOINT \
+  --reset-optimizer --reset-dataloader --reset-meters --reset-lr-scheduler \
   --max-update $TOTAL_UPDATES &> "$DIR_OUTPUT/training-$(date +"%Y-%m-%d_%H-%M-%S").log"
 #   --num-workers 4 \
